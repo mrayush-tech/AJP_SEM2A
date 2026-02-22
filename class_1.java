34519b7608b70e76f5eb632b95d92bc3ce2b06ab
mrayush-tech <mr100ayush@gmail.com>
2026-02-22 17:38:38 +0530

    Add files via upload

---
+++ class_1.java
@@ -0,0 +1,15 @@
+public static void main(String[] args) {
+    
+}+
+interface calc{
+    void calculate(int n1, int n2);
+default  void calculate(int n1, int n2,int n3);
+int s = n1+n2+n3;
+System.out.println("sum=:" +s);
+
+}
+class sum implements calc{
+public static calculate(int n1, int n2) {
+    
+}
+}
